3168202bd9c9ab75ec06a43d4c6fbc1fba836972
Ivan <i.protzai@gmail.com>
2024-04-17 23:52:19 +0300

    lab updated

--- assemblyLangLab01/main.cpp
+++ assemblyLangLab01/main.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
 using namespace std;
 
-char FORMAT[] = "%s %s\n";
-char HELLO[] = "Hello";
-char WORLD[] = "world";
+char FORMAT[] = "%s %s %s %s %s %s";
+char DESIGN[] = "Design";
+char BY[] = "by";
+char IVAN[] = "Ivan";
+char PROTSAI[] = "Protsai";
+char IN[] = "in";
+char YEAR[] = "2024";
 
 int main() 
 {
 	__asm 
 	{ 		
-		mov eax, offset WORLD
+		mov eax, offset YEAR
 		push eax
-		mov eax, offset HELLO
+		mov eax, offset IN
+		push eax
+		mov eax, offset PROTSAI
+		push eax
+		mov eax, offset IVAN
+		push eax
+		mov eax, offset BY
+		push eax
+		mov eax, offset DESIGN
 		push eax
 		mov eax, offset FORMAT
 		push eax
@@ -20,8 +32,41 @@ int main()
 
 		pop ebx
 		pop ebx
+		pop ebx 
+		pop ebx
+		pop ebx
+		pop ebx
 		pop ebx
 	} 
+	std::cout << std::endl;
 	system("pause");
 	return 0;
 }
+
+//#include <iostream>
+//using namespace std;
+//
+//char FORMAT[] = "%s %s\n";
+//char HELLO[] = "Hello";
+//char WORLD[] = "world";
+//
+//int main()
+//{
+//	__asm
+//	{
+//		mov eax, offset WORLD
+//		push eax
+//		mov eax, offset HELLO
+//		push eax
+//		mov eax, offset FORMAT
+//		push eax
+//		mov edi, printf
+//		call edi
+//
+//		pop ebx
+//		pop ebx
+//		pop ebx
+//	}
+//	system("pause");
+//	return 0;
+//}
